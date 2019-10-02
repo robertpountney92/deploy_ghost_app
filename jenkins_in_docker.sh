fcacfc75b41ac67b28d2a48b45981b295dc15422
robertpountney92 <robertpountney92@gmail.com>
2019-10-02 20:08:49 +0100

    Setup jenkins in docker script

---
+++ jenkins_in_docker.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+#Setup jenkin in docker container
+docker pull jenkinsci/blueocean
+docker run \
+  --rm \
+  -d \
+  -u root \
+  -p 8081:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean
